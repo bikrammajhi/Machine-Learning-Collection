d1870ff4c83a8dfdfdea78218802d1e202e8cb1c
Bikram Majhi <115028900+bikrammajhi@users.noreply.github.com>
2024-04-13 21:43:08 +0530

    Update config.py
    Modified path for kaggle

--- ML/Pytorch/GANs/CycleGAN/config.py
+++ ML/Pytorch/GANs/CycleGAN/config.py
@@ -3,8 +3,9 @@ import albumentations as A
 from albumentations.pytorch import ToTensorV2
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-TRAIN_DIR = "data/train"
-VAL_DIR = "data/val"
+TRAIN_DIR = "/kaggle/input/isicdata/Assignment_4/Train/Train_data"
+VAL_DIR = "/kaggle/input/isicdata/Assignment_4/Test/Test"
+
 BATCH_SIZE = 1
 LEARNING_RATE = 1e-5
 LAMBDA_IDENTITY = 0.0
@@ -18,12 +19,7 @@ CHECKPOINT_GEN_Z = "genz.pth.tar"
 CHECKPOINT_CRITIC_H = "critich.pth.tar"
 CHECKPOINT_CRITIC_Z = "criticz.pth.tar"
 
-transforms = A.Compose(
-    [
-        A.Resize(width=256, height=256),
-        A.HorizontalFlip(p=0.5),
-        A.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5], max_pixel_value=255),
-        ToTensorV2(),
-    ],
-    additional_targets={"image0": "image"},
-)
+transforms = transforms.Compose([
+    transforms.Resize((64, 64)),
+    transforms.ToTensor()
+])
